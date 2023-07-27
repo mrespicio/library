bfeaf4bec060a51339c8ae2cdb5955174862605f
mrespicio <meganrespicio@hotmail.com>
2023-07-27 15:57:35 -0700

    create Book object constructor

---
+++ script.js
@@ -0,0 +1,16 @@
+// store book objects
+let myLibrary = [];
+
+function Book(title, author, pages, status){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.info = function(){
+        return `${title} by ${author}, ${pages} pages, ${status}`;
+    }
+}
+
+function addBook(){
+
+}
