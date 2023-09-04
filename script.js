5ff6ce7721d85c07a72e86a6aec8404dee5066e4
mrespicio <meganrespicio@hotmail.com>
2023-09-04 15:04:58 -0700

    add delete row button

--- script.js
+++ script.js
@@ -6,17 +6,7 @@ function Book(title, author, pages, status){
     this.author = author;
     this.pages = pages;
     this.status = status;
-    // this.info = function(){
-    //     return `${title} by ${author}, ${pages} pages, ${status}`;
-    // }
 }
-
-// completed, not read, in progress, plan to read
-
-let modal = document.getElementById('add-modal');
-let btn = document.getElementById('add-btn');
-let span = document.getElementsByClassName('close')[0];
-
 
 let mockingbird = new Book('To Kill A Mockingbird', 'Harper Lee', '281', 'Read');
 let cuckoo = new Book('One Flew Over the Cuckoo\'s Nest', 'Ken Kesey', '320', 'Read');
@@ -41,6 +31,13 @@ let authorInput = document.getElementById('author');
 let pagesInput = document.getElementById('pages');
 let statusInput = document.getElementById('status');
 
+let dropdown = document.querySelectorAll('.dropdown');
+// dropdown.forEach(() => 
+//     statusInput.addEventListener('change', (option) =>{
+//         console.log(option.target.value);
+//     }))
+
+
 btnAdd.addEventListener('click', () => {
     let book = new Book(titleInput.value, authorInput.value, 
         pagesInput.value, statusInput.value);
@@ -51,14 +48,32 @@ btnAdd.addEventListener('click', () => {
     let authorCell = row.insertCell(1);
     let pagesCell = row.insertCell(2);
     let statusCell = row.insertCell(3);
+    let actionCell = row.insertCell(4);
 
     titleCell.innerHTML = book.title;
     authorCell.innerHTML = book.author;
     pagesCell.innerHTML = book.pages;
+    // statusCell.innerHTML = statusDropdown(book.status);
     statusCell.innerHTML = book.status;
+
+    actionCell.innerHTML = `<a>Edit</a> <button class="dlt-btn" onclick="deleteRow(this)" >Delete</button>`
 
     clearFields();
 });
+
+function deleteRow(row){
+    table.deleteRow(this);
+}
+
+// let dltBtns = document.querySelectorAll('.dlt-btn');
+// dltBtns.forEach(btn => {
+//     btn.addEventListener('click', () =>{
+//         //console.log('the row index is : ' + e.rowIndex)
+//         console.log('you clicked on delete');
+//     })
+// }) // delete
+
+// edit button makes fields editable
 
 function clearFields(){
     titleInput.value = '';
@@ -66,14 +81,13 @@ function clearFields(){
     pagesInput.value = '';
 }
 
-
-// function addRow(obj){
-//     let format = ` <tr>
-//         <td scope="row">${obj.title}</td>
-//         <td>${obj.author}</td>
-//         <td>${obj.pages}</td>
-//         <td>${obj.status}</td>
-//         <tr>`;
-//     return format;
-// }
-
+function statusDropdown(selected){
+    // selected is the status
+    let dropdown = 
+    `<select name="status" id="status">
+        <option value="Plan to read">Plan to read</option>
+        <option value="Reading">Reading</option>
+        <option selected value="Completed">Completed</option>
+    </select>`;
+    return dropdown;
+}
